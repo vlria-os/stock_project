4c4f9a7103d3e3284cd14bc0cef69ab2797a5990
novolunteeer <novolunteeer@gmail.com>
2026-05-14 21:36:48 +0900

    trade-service

---
+++ trade-service/src/main/java/com/example/demo/order/enums/Status.java
@@ -0,0 +1,5 @@
+package com.example.demo.order.enums;
+
+public enum Status {
+    PENDING, PARTIALLY_FILLED, FILLED, CANCELLED
+}
---
+++ trade-service/src/main/java/com/example/demo/trade/entity/TradeHistory.java
@@ -0,0 +1,53 @@
+package com.example.demo.trade.entity;
+
+import com.example.demo.order.enums.OrderCondition;
+import com.example.demo.order.enums.OrderType;
+import com.example.demo.order.enums.Side;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class TradeHistory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long userId;
+
+    @Column(length = 20, nullable = false)
+    private String stockCode;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Side side;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private OrderType orderType;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private OrderCondition orderCondition;
+
+    @Column(nullable = false)
+    private Long quantity;
+
+    @Column(nullable = false)
+    private Long price;
+
+    @Column(nullable = false)
+    private Long totalAmount;
+
+    @Column(nullable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+}
