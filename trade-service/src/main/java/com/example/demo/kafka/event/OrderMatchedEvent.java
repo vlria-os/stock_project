624cc0a0dcce4cf02c619d8cdd39a3023bfaa102
novolunteeer <novolunteeer@gmail.com>
2026-05-17 23:48:00 +0900

    redis

---
+++ trade-service/src/main/java/com/example/demo/kafka/event/OrderMatchedEvent.java
@@ -0,0 +1,14 @@
+package com.example.demo.kafka.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class OrderMatchedEvent {
+
+}
