ce2ac900548dd1adb2ab56a42db8b931bb5870f1
novolunteeer <novolunteeer@gmail.com>
2026-06-02 22:15:23 +0900

    orderbook ws

--- react/src/page/trade/OrderBook.jsx
+++ react/src/page/trade/OrderBook.jsx
@@ -9,12 +9,40 @@ const OrderBook = ({ stockCode }) => {
   useEffect(() => {
     //1. websocket 연결
     const client=new Client({
-        webSocketFactory: () => new SockJS()
-    })
-  })
+        webSocketFactory: () => new SockJS(`${import.meta.env.VITE_TRADE_SERVICE_URL}/ws`),
+        onConnect: () => {
+            //2. 호가 구독
+            client.subscribe("/topic/orderbook", (message) => {
+                setOrderBook(JSON.parse(message.body));
+            });
+
+            //3. 백엔드에 kis 구독 요청
+            fetch(`/api/trade/orderbook/subscribe/${stockCode}`, {
+                method: 'POST'
+            });
+        },
+        onDisconnect: () => {
+            console.log("WebSocket 연결 종료");
+        },
+    });
+
+    client.activate();
+    setStompClient(client);
+
+    //페이지 이탈 시 구독 해제
+    return () => {
+        fetch(`/api/trade/orderbook/unsubscribe/${stockCode}`, {
+            method: 'POST'
+        });
+
+        client.deactivate();
+    };
+  }, [stockCode]);
 
   return (
-    <div>OrderBook</div>
+    <div>
+        
+    </div>
   )
 }
 
