0ae107114868aa51943b34e2eaefacd970b87e59
novolunteeer <novolunteeer@gmail.com>
2026-06-03 14:44:52 +0900

    websocket cors

--- trade-service/src/main/java/com/example/demo/config/WebSocketConfig.java
+++ trade-service/src/main/java/com/example/demo/config/WebSocketConfig.java
@@ -29,6 +29,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
+                .setAllowedOrigins("http://13.124.206.135")
                 .withSockJS();
     }
 }
