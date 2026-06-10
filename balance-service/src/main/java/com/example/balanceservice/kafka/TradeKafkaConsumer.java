7284555521c31397c613d5213691c3bbd6b916d0
novolunteeer <novolunteeer@gmail.com>
2026-06-10 20:54:49 +0900

    trade request kafka

--- balance-service/src/main/java/com/example/balanceservice/kafka/TradeKafkaConsumer.java
+++ balance-service/src/main/java/com/example/balanceservice/kafka/TradeKafkaConsumer.java
@@ -5,14 +5,18 @@ import com.example.balanceservice.service.BalanceService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
+import tools.jackson.databind.ObjectMapper;
 
 @Component
 @RequiredArgsConstructor
 public class TradeKafkaConsumer {
     private final BalanceService balanceService;
+    private final ObjectMapper objectMapper;
 
     @KafkaListener(topics = "balance.trade.request", groupId = "balance-service")
-    public void getTradeRequest(TradeRequest request){
+    public void getTradeRequest(String message){
+        TradeRequest request = objectMapper.readValue(message, TradeRequest.class);
+
         balanceService.executeTrade(
                 request.getBuyOrderId(),
                 request.getBuyerId(),
