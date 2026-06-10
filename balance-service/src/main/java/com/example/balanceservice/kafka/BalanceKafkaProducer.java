9d4f536f71d91b95e4348bd8fa6b5e953c1991f3
novolunteeer <novolunteeer@gmail.com>
2026-06-10 21:46:50 +0900

    trade request kafka

--- balance-service/src/main/java/com/example/balanceservice/kafka/BalanceKafkaProducer.java
+++ balance-service/src/main/java/com/example/balanceservice/kafka/BalanceKafkaProducer.java
@@ -1,16 +1,17 @@
 package com.example.balanceservice.kafka;
 
 import com.example.balanceservice.event.TradeResultEvent;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
-import tools.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectMapper; // 올바른 import
 
 @Component
 @RequiredArgsConstructor
 public class BalanceKafkaProducer {
     private final KafkaTemplate<String, Object> kafkaTemplate;
-    private final ObjectMapper objectMapper;
+    private ObjectMapper objectMapper;
 
     public void sendTradeResult(Long buyOrderId,Long buyerId, Long sellerId, Long amount, boolean success) {
         TradeResultEvent event = TradeResultEvent.builder()
@@ -22,6 +23,11 @@ public class BalanceKafkaProducer {
                 .build();
 
         String topic = success ? "balance.trade.success" : "balance.trade.fail";
-        kafkaTemplate.send(topic, objectMapper.writeValueAsString(event));
+
+        try {
+            kafkaTemplate.send(topic, objectMapper.writeValueAsString(event));
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
