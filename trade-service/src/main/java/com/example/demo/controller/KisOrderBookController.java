1a29a61799a834610bc33dbb84a4a9c29468cef2
novolunteeer <novolunteeer@gmail.com>
2026-06-10 13:30:09 +0900

    orderbook controller

--- trade-service/src/main/java/com/example/demo/controller/KisOrderBookController.java
+++ trade-service/src/main/java/com/example/demo/controller/KisOrderBookController.java
@@ -2,12 +2,14 @@ package com.example.demo.controller;
 
 import com.example.demo.websocket.KisWebSocketClient;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/trade/orderbook")
@@ -16,6 +18,7 @@ public class KisOrderBookController {
 
     @PostMapping("/subscribe/{stockCode}")
     public ResponseEntity<Void> subscribe(@PathVariable String stockCode){
+        log.info("구독 요청: {}", stockCode);
         kisWebSocketClient.subscribe(stockCode);
         return ResponseEntity.ok().build();
     }
