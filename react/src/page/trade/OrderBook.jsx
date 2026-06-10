329eaf6c2946c424e89130df9858375f6157235b
novolunteeer <novolunteeer@gmail.com>
2026-06-10 14:48:29 +0900

    orderbook

--- react/src/page/trade/OrderBook.jsx
+++ react/src/page/trade/OrderBook.jsx
@@ -44,11 +44,31 @@ const OrderBook = ({ stockCode }) => {
   }, [stockCode]);
 
   return (
-    <div>
+    <div style={{ display: 'flex', flexDirection: 'column', width: '300px', fontFamily: 'monospace' }}>
         {orderBook ? (
-                <pre>{JSON.stringify(orderBook, null, 2)}</pre>
+            <>
+                <div style={{ textAlign: 'center', marginBottom: '8px' }}>
+                    {orderBook.stockCode} {orderBook.time}
+                </div>
+
+                {/* 매도 호가 - 높은 가격부터 */}
+                {[...orderBook.askPrices].reverse().map((ask, i) => (
+                    <div key={i} style={{ display: 'flex', justifyContent: 'space-between', backgroundColor: '#e8f4f8', padding: '2px 8px' }}>
+                        <span style={{ color: 'blue' }}>{Number(ask.volume).toLocaleString()}</span>
+                        <span>{Number(ask.price).toLocaleString()}</span>
+                    </div>
+                ))}
+
+                {/* 매수 호가 */}
+                {orderBook.bidPrices.map((bid, i) => (
+                    <div key={i} style={{ display: 'flex', justifyContent: 'space-between', backgroundColor: '#fff0f0', padding: '2px 8px' }}>
+                        <span>{Number(bid.price).toLocaleString()}</span>
+                        <span style={{ color: 'red' }}>{Number(bid.volume).toLocaleString()}</span>
+                    </div>
+                ))}
+            </>
         ) : (
-                <p>호가 데이터 로딩 중...</p>
+            <p>호가 데이터 로딩 중...</p>
         )}
     </div>
   )
