d9fdb7c7ac533348ddc3c98814d48ea6d15b5cde
novolunteeer <novolunteeer@gmail.com>
2026-06-12 17:20:37 +0900

    stock code trade

--- stock-service/src/main/java/com/example/demo/controller/TradeStockController.java
+++ stock-service/src/main/java/com/example/demo/controller/TradeStockController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/stock")
@@ -34,9 +36,9 @@ public class TradeStockController {
     }
 
     @GetMapping("/code")
-    public ResponseEntity<?> getStockCode(@RequestParam("stockName") String stockName){
+    public ResponseEntity<?> getStockCode(@RequestParam("stockName") String name){
         try {
-            return ResponseEntity.ok(stockService.getStockCode(stockName));
+            return ResponseEntity.ok(Map.of("code", stockService.getStockCode(name)));
         } catch (Exception e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
