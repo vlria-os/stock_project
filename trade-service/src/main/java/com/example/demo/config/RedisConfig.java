62f6f2eb24727fe99cf3e70cf3a2998bf83c3d79
novolunteeer <novolunteeer@gmail.com>
2026-06-17 15:18:16 +0900

    redisConfig

--- trade-service/src/main/java/com/example/demo/config/RedisConfig.java
+++ trade-service/src/main/java/com/example/demo/config/RedisConfig.java
@@ -1,14 +1,29 @@
 package com.example.demo.config;
 
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
+    @Value("${spring.data.redis.host}")
+    private String redisHost;
+
+    @Bean(destroyMethod = "shutdown")
+    public RedissonClient redissonClient() {
+        Config config = new Config();
+        config.useClusterServers()
+                .addNodeAddress("rediss://" + redisHost + ":6379")
+                .setScanInterval(2000)
+                .setSslEnableEndpointIdentification(false);
+        return Redisson.create(config);
+    }
 
     @Bean
     public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory factory){
