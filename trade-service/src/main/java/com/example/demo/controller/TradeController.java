cf87ab698da426262c5ec069d588895e833e2277
novolunteeer <novolunteeer@gmail.com>
2026-06-19 18:31:41 +0900

    sse

--- trade-service/src/main/java/com/example/demo/controller/TradeController.java
+++ trade-service/src/main/java/com/example/demo/controller/TradeController.java
@@ -109,10 +109,7 @@ public class TradeController {
     }
 
     @GetMapping(value = "/sse/connect", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public SseEmitter connect(@RequestHeader(value = "X-User-Id", required = false) Long userId){
-        if (userId == null){
-            userId = 15L;
-        }
+    public SseEmitter connect(@RequestHeader("X-User-Id") Long userId){
         return sseEmitterService.connect(userId);
     }
 }
